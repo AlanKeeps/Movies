fdeb7a0b92a86d9c68ae5bc326ae6ee689a71b49
AlanKeeps <alan.keeps.walking@gmail.com>
2022-11-03 00:31:17 +0300

    use query annotations

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -2,9 +2,10 @@ package com.example.movies;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("movie?token=G0GS1WP-FQ14QY9-MHM51EH-AV05TEH&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&page2&limit=10")
-    Single<MovieResponse> loadMovies();
+    @GET("movie?token=G0GS1WP-FQ14QY9-MHM51EH-AV05TEH&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=10")
+    Single<MovieResponse> loadMovies(@Query("page") int page);
 }
