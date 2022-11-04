c36c4baedd6aeaa10cc293d529ce4dd13b9dbe01
AlanKeeps <alan.keeps.walking@gmail.com>
2022-11-05 01:18:35 +0300

    fixed loading troubles, and remove loadmovie method to viewmodel

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -6,7 +6,6 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-//    @GET("movie?token=G0GS1WP-FQ14QY9-MHM51EH-AV05TEH&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=10")
-    @GET("movie?token=G0GS1WP-FQ14QY9-MHM51EH-AV05TEH&field=rating.kp&search=4-8&sortField=votes.kp&sortType=-1&limit=100")
+    @GET("movie?token=G0GS1WP-FQ14QY9-MHM51EH-AV05TEH&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=30")
     Single<MovieResponse> loadMovies(@Query("page") int page);
 }
--- app/src/main/java/com/example/movies/MoviesAdapter.java
+++ app/src/main/java/com/example/movies/MoviesAdapter.java
@@ -19,6 +19,11 @@ import java.util.List;
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
 
     private List<Movie> movies = new ArrayList<>();
+    private OnReachEndListener onReachEndListener;
+
+    public void setOnReachEndListener(OnReachEndListener onReachEndListener) {
+        this.onReachEndListener = onReachEndListener;
+    }
 
     public void setMovies(List<Movie> movies) {
         this.movies = movies;
@@ -53,6 +58,15 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         Drawable background = ContextCompat.getDrawable(holder.itemView.getContext(), backgroundId);
         holder.textViewRating.setBackground(background);
         holder.textViewRating.setText(String.valueOf(rating));
+
+        if (position >= movies.size() - 10 && onReachEndListener != null) {
+            onReachEndListener.onReachEnd();
+        }
+    }
+
+    interface OnReachEndListener {
+
+        void onReachEnd();
     }
 
     @Override
