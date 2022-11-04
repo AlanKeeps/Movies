17b82c6ce2e00a8ac1aa02e7bbe1512fe307d3aa
AlanKeeps <alan.keeps.walking@gmail.com>
2022-11-04 23:55:35 +0300

    add circle background for textview

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -6,6 +6,7 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("movie?token=G0GS1WP-FQ14QY9-MHM51EH-AV05TEH&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=10")
+//    @GET("movie?token=G0GS1WP-FQ14QY9-MHM51EH-AV05TEH&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=10")
+    @GET("movie?token=G0GS1WP-FQ14QY9-MHM51EH-AV05TEH&field=rating.kp&search=4-8&sortField=votes.kp&sortType=-1&limit=100")
     Single<MovieResponse> loadMovies(@Query("page") int page);
 }
